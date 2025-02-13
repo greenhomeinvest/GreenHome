538468d792633a91a684444a703d14cfa9e47870
greenhomeinvest <greenhomeinvestltd@gmail.com>
2025-02-13 10:55:40 +0200

    change pagination

--- listings/urls.py
+++ listings/urls.py
@@ -6,7 +6,7 @@ from listings import views
 urlpatterns = [
     path('all',views.listings , name='listings'),
     path('<int:id>',views.current_listing , name='current_listing'),
-    path('search',views.search , name='search'),
+    path('search?city=',views.search , name='search'),
     path('realtor/<int:realtor_id>', views.realtor_listings, name='realtor_listings'),
     path('get-states/', views.get_states, name='get_states'),
 ]

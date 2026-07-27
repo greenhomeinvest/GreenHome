b6e122cb129a34cbb530a44f6c3c2abd6ffdccbd
Yanko Rashkov <yanko.rashkov@gmail.com>
2026-07-27 14:45:11 +0300

    Added hot fix for static

--- GreenHome/urls.py
+++ GreenHome/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.sites.AdminSite.site_header = 'Green Home'
 admin.sites.AdminSite.index_title = 'Администрация'
@@ -30,4 +31,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+    ]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
